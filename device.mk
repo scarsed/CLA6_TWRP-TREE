235d771c3edc7510335795839fd5375284184cfc
scarsed <kimaniwilber@gmail.com>
2025-12-17 22:16:39 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+PRODUCT_COPY_FILES += 
+system/core/rootdir/etc/init.zygote64_32.rc:$(TARGET_COPY_OUT_RAMDISK)/init.zygote64_32.rc
 
 # Inherit from mt6789-common
 $(call inherit-product, device/transsion/mt6789-common/common.mk)
