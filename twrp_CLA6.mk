d8aab5c1f425290c5cfcf75e13257e0e068db20e
scarsed <kimaniwilber@gmail.com>
2025-12-17 23:05:09 +0300

    Update twrp_CLA6.mk

--- twrp_CLA6.mk
+++ twrp_CLA6.mk
@@ -4,16 +4,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
-# Inherit from TECNO-CLA6 device
+# Inherit from Tecno-CLA6 device
 $(call inherit-product, device/tecno/CLA6/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := CLA6
 PRODUCT_NAME := twrp_CLA6
